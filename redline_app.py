271cff7b7e41395ea3adbedb7b695372c22ff569
Glitchingreality <Glitchingreality@gmail.com>
2026-01-23 14:11:56 -0500

    Update redline_app.py

--- redline_app.py
+++ redline_app.py
@@ -31,7 +31,7 @@ TIMELINE = defaultdict(list)
 # UI SETUP
 # -------------------------
 st.set_page_config(page_title="Redline Threat Hunter", layout="wide")
-st.title("❗Redline❗ Threat Hunter 🚨 ")
+st.title("❗Redline❗ Threat Hunter 🕵️‍♂️🥷 ")
 st.markdown(
     "Analyze log files for suspicious activity using execution chains, LOLBIN detection, and behavioral correlation."
 )
@@ -193,5 +193,6 @@ if TIMELINE:
         file_name="redline_analysis.csv",
         mime="text/csv",
     )
+
 
 
